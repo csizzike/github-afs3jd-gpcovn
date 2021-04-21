7d5562e6a9d7dea04a7560b53fe570b88cac8a09
csizzike <csizzike@gmail.com>
2021-04-21 10:36:50 +0200

    1 file modified, 1 added

--- src/app/author-list/author.ts
+++ src/app/author-list/author.ts
@@ -1,31 +1,37 @@
 export class AuthorTable {
   public static authors = [
     {
+      id: 1,
       name: "Tolkien, J.R.",
       born: 1892,
       nation: "UK"
     },
     {
+      id: 2,
       name: "Eric Elliott",
       born: 1976,
       nation: "FR"
     },
     {
+      id: 3,
       name: "Glenn Block, et al.",
       born: 1973,
       nation: "USA"
     },
     {
+      id: 4,
       name: "Addy Osmani",
       born: 1981,
       nation: "USA"
     },
     {
+      id: 5,
       name: "Nicholas C. Zakas",
       born: 1986,
       nation: "RU"
     },
     {
+      id: 6,
       name: "Sir Arthur Conan Doyle",
       born: 1859,
       nation: "UK"
---
+++ src/app/bookstore-list/bookstore.ts
@@ -0,0 +1,20 @@
+export class BookstoreTable {
+  public static bookstores = [
+    {
+      id: 1,
+      name: "Amazing bookstore for young nerds",
+      city: "New York",
+      post: "94321",
+      street: "Paddington Street 91/B",
+      open: "8:00 - 16:00"
+    },
+    {
+      id: 2,
+      name: "Tea and Books",
+      city: "London",
+      post: "22331",
+      street: "221B Baker Street",
+      open: "9:00 - 17:00"
+    }
+  ];
+}
